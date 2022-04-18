a2f8a347de92e5712e1f28af34b4fe57412f6488
Dominik Rubröder <dominik.rubroeder@icloud.com>
2022-04-18 15:07:25 +0200

    Initial Commit

---
+++ instagram/instagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  instagramApp.swift
+//  instagram
+//
+//  Created by Dominik Rubröder on 18.04.22.
+//
+
+import SwiftUI
+
+@main
+struct instagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
